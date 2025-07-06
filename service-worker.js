7dcf5bcc6483f60007ecb97bf6173a255976a1b4
Kyle Stratton <kyle.stratton@icloud.com>
2025-07-06 20:05:00 +0100

    fix pwa

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
 const CACHE_NAME = "my-cache-v1";
 const urlsToCache = [
   "/",
-  "index.html",
-  "styles.css",
-  "script.js",
-  "assets/logo.png",
+  "./index.html",
+  "./styles.css",
+  "./script.js",
+  "./assets/logo.png",
 ];
 
 
