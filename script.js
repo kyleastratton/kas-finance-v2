2d7c3c9d69634ccec0f701fd92d48c764fd373d9
Kyle Stratton <kyle.stratton@icloud.com>
2025-07-06 19:51:43 +0100

    add pwa support

--- script.js
+++ script.js
@@ -1592,3 +1592,12 @@ function filterLogos() {
       grid.appendChild(img);
     });
 }
+
+// PWA support
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/service-worker.js")
+      .then((reg) => console.log("Service Worker registered", reg))
+      .catch((err) => console.error("Service Worker registration failed", err));
+  });
+}
