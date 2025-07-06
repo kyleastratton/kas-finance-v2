b042e0a53045fcf7685cf956e9a17c2a8a894d7a
Kyle Stratton <kyle.stratton@icloud.com>
2025-07-06 20:02:24 +0100

    add pwa support

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
 const CACHE_NAME = "my-cache-v1";
 const urlsToCache = [
   "/",
-  "/index.html",
-  "/styles.css",
-  "/script.js",
-  "/icons/icon-192.png",
-  "/icons/icon-512.png"
+  "index.html",
+  "styles.css",
+  "script.js",
+  "assets/logo.png",
 ];
+
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
